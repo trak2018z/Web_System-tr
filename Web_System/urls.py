6e291aa34185fb5ede3d5f89fa419bed1cbb3fa8
Tomel23 <tomel23@wp.pl>
2017-11-20 19:56:41 +0100

    Utworzenie panelu logowania, rejestracji, oraz modułu informacyjnego

--- Web_System/urls.py
+++ Web_System/urls.py
@@ -13,9 +13,40 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from datetime import datetime
+import django.contrib.auth.views
+
+import Info_app.views
+import Login_app.forms
+import Login_app.views
 from django.contrib import admin
+from django.conf.urls import url, include
+
+admin.autodiscover()
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', Info_app.views.home, name='home'),
+    url(r'^contact$', Info_app.views.contact, name='contact'),
+    url(r'^about', Info_app.views.about, name='about'),
+    url(r'^login/$',
+        django.contrib.auth.views.login,
+        {
+            'template_name': 'Login_app/login.html',
+            'authentication_form': Login_app.forms.BootstrapAuthenticationForm,
+            'extra_context':
+                {
+                    'title': 'Logowanie',
+                    'year': datetime.now().year,
+                }
+        },
+        name='login'),
+    url(r'^logout$',
+        django.contrib.auth.views.logout,
+        {
+            'next_page': '/',
+        },
+        name='logout'),
+    url(r'^singup/$', Login_app.views.singup, name='singup'),
+
 ]
