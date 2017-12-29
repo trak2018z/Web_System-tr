f4f9e713622715815843ea8073ef5a4b73cd19d4
Tomel23 <tomel23@wp.pl>
2017-12-29 02:47:56 +0100

    Edycja, przegladanie zlecen przy pomocy widoku generycznego wersja 3(edycji adresow- problem z modalem)

--- Zlecenie_app/urls.py
+++ Zlecenie_app/urls.py
@@ -22,7 +22,8 @@ admin.autodiscover()
 
 urlpatterns = [
     url(r'^$', views.ZleceniaListView.as_view(), name='lista_zlecen'),
-    url(r'^(?P<pk>[0-9]+)/edytuj/$', views.ZlecenieUpdateView.as_view(), name='edytuj_zlecenie'),
     url(r'^(?P<pk>[0-9]+)', views.ZleceniaDetailView.as_view(), name='szczegoly_zamowienia'),
-    # url(r'^(?P<pk>[0-9]+)/edytuj adres/$', views.AdresUpdateView.as_view(), name='edytuj_adres'),
+    url(r'^(?P<pk>[0-9]+)/edytuj/$', views.ZlecenieUpdateView.as_view(), name='edytuj_zlecenie'),
+
+    url(r'^(?P<pk>[0-9]+)/edytuj_adres/(?P<adres_id>[0-9]+)/$', views.AdresUpdateView.as_view(), name='edytuj_adres'),
 ]
--- Zlecenie_app/views.py
+++ Zlecenie_app/views.py
@@ -1,10 +1,15 @@
 """
 Definition of views.
 """
-
+import googlemaps
 from django.core.urlresolvers import reverse_lazy
+from django.shortcuts import render
 from django.views import generic
+from googlemaps import distance_matrix
 
+from Adres_app.models import Adres
+from Zlecenie_app.forms import ZleceniaForm, AdresForm
+# from Zlecenie_app.forms import ZleceniaForm, AdresForm
 from .models import Zlecenie
 
 
@@ -21,6 +26,7 @@ class ZleceniaListView(generic.ListView):
 
 class ZleceniaDetailView(generic.DetailView):
     model = Zlecenie
+
     # template_name = 'Zlecenie_app/tmp.html'
 
 
@@ -62,3 +68,83 @@ class ZlecenieUpdateView(generic.UpdateView):
         return reverse_lazy('Zlecenie_app:szczegoly_zamowienia', kwargs={'pk': self.object.pk})
 
 
+class AdresUpdateView(generic.UpdateView):
+    model = Adres
+    template_name = 'Zlecenie_app/adres_form.html'
+    form_class = AdresForm
+
+    def get_success_url(self):
+        return reverse_lazy('Zlecenie_app:szczegoly_zamowienia', kwargs={'pk': self.object.pk})
+
+
+def _get_form(request, formcls, prefix):
+    data = request.POST if prefix in request.POST else None
+    return formcls(data, prefix=prefix)
+
+
+class ZlecenieUpdateView2(generic.UpdateView):
+    template_name = 'Zlecenie_app/zlecenie_form.html'
+
+    def get_initial(self):
+        gmap = googlemaps.Client(key='AIzaSyAewSjMOfSLupwSWSJvkpvR4-WD5t59K1Y')
+        dit = distance_matrix.distance_matrix(gmap, 'Stalowa Wola', 'Sandomierz')
+        dist = dit['rows'][0]['elements'][0]['duration']['text']
+        return {
+            'data_dostarczenia': dist
+        }
+
+    def get(self, request, *args, **kwargs):
+        return self.render_to_response(
+            {'adresform': AdresForm(prefix='adresform_pre'), 'zlecenieform': ZleceniaForm(prefix='zlecenieform_pre')})
+
+    def post(self, request, *args, **kwargs):
+        adresform = _get_form(request, AdresForm, 'aform_pre')
+        zlecenieform = _get_form(request, ZleceniaForm, 'bform_pre')
+        if adresform.is_bound and adresform.is_valid():
+            adresform.save()
+        # Process adresform and render response
+        elif zlecenieform.is_bound and zlecenieform.is_valid():
+            zlecenieform.save()
+        # Process zlecenieform and render response
+        return self.render_to_response({'adresform': adresform, 'zlecenieform': zlecenieform})
+
+
+class ZlecenieCreateView(generic.CreateView):
+    model = Zlecenie
+    template_name = 'Zlecenie_app/zlecenie_create.html'
+    success_url = reverse_lazy('Zlecenie_app:lista_zlecen')
+    fields = ['adres_odbioru', 'adres_dostawy']
+
+    def get_initial(self):
+        gmap = googlemaps.Client(key='AIzaSyAewSjMOfSLupwSWSJvkpvR4-WD5t59K1Y')
+        dit = distance_matrix.distance_matrix(gmap, 'Stalowa Wola', 'Sandomierz')
+        dist = dit['rows'][0]['elements'][0]['duration']['text']
+        return {
+            'data_dostarczenia': dist
+        }
+
+    def form_valid(self, form):
+        user = self.request.user
+
+        form.instance.user = user
+        self.object = form.save()
+        return render(self.request, {'zlecenie': self.object})
+
+    template_name = 'Zlecenie_app/zlecenie_form.html'
+
+    def get(self, request, *args, **kwargs):
+        return self.render_to_response(
+            {'adresform': AdresForm(prefix='adresform_pre'), 'zlecenieform': ZleceniaForm(prefix='zlecenieform_pre')})
+
+    def post(self, request, *args, **kwargs):
+
+        adresform = _get_form(request, AdresForm, 'aform_pre')
+
+        zlecenieform = _get_form(request, ZleceniaForm, 'bform_pre')
+        if adresform.is_bound and adresform.is_valid():
+            adresform.save()
+        # Process adresform and render response
+        elif zlecenieform.is_bound and zlecenieform.is_valid():
+            zlecenieform.save()
+            # Process zlecenieform and render response
+        return self.render_to_response({'adresform': adresform, 'zlecenieform': zlecenieform})
