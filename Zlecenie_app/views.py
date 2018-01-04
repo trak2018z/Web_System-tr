e6bf14bb5f30091596ea4c82f3905c0b8907a2a9
Tomel23 <tomel23@wp.pl>
2018-01-04 15:03:39 +0100

    Naprawa błedow związanych z edycja zamowien

--- Zlecenie_app/views.py
+++ Zlecenie_app/views.py
@@ -16,11 +16,13 @@ from Zlecenie_app.forms import ZleceniaForm, AdresForm
 from .models import Zlecenie
 
 
+# dzialajace
 @method_decorator(login_required, name='dispatch')
 class ZleceniaListView(generic.ListView):
     template_name = 'Zlecenie_app/zlecenie_list.html'
     context_object_name = 'lista_zlecen'
     login_required = True
+
     def get_queryset(self):
         if not self.request.user.groups.filter(name='Klient').exists():
             return Zlecenie.objects.all()
@@ -40,22 +42,14 @@ class ZleceniaDetailView(generic.DetailView):
         else:
             self.template_name = 'Zlecenie_app/zlecenie_detail.html'
             return Zlecenie.objects.filter(user=self.request.user)
-            # dla operatora
-            # class AdresCreateView(generic.CreateView):
-            #     model = Adres
-            #     template_name = 'Adres_app/adres_form.html'
-            #     success_url = reverse_lazy('Adres_app:lista_adresow')
-            #     fields = ['nazwa', 'ulica', 'nr', 'kod_pocztowy', 'miasto']
-            #
-            #     def form_valid(self, form):
-            #         user = self.request.user
-            #         form.instance.user = user
-            #         self.object = form.save()
-            #         return render(self.request, 'Adres_app/adres_list.html', {'adres': self.object})
 
-            # class ZleceniaDetailView(generic.DetailView):
-            #   model = Zlecenie
-            # template_name = 'Zlecenie_app/tmp.html'
+
+@method_decorator(login_required, name='dispatch')
+class ZlecenieCreate_newAdesView(generic.CreateView):
+    model = Adres
+    template_name = 'Zlecenie_app/zlecenie_form.html'
+    success_url = reverse_lazy('Zlecenie_app:nowe_zlecenie')
+    fields = ['nazwa', 'ulica', 'nr', 'kod_pocztowy', 'miasto']
 
 
 @method_decorator(login_required, name='dispatch')
@@ -69,14 +63,7 @@ class ZlecenieUpdateView(generic.UpdateView):
         return reverse_lazy('Zlecenie_app:szczegoly_zamowienia', kwargs={'pk': self.object.pk})
 
 
-# adres updatecccccccccccccccc
-# class AdresUpdateView(generic.UpdateView):
-#     model = Adres
-#     template_name = 'Zlecenie_app/adres_form.html'
-#     form_class = AdresForm
-#
-#     def get_success_url(self):
-#         return reverse_lazy('Zlecenie_app:szczegoly_zamowienia', kwargs={'pk': self.object.pk})
+# do poprawki
 @method_decorator(login_required, name='dispatch')
 class ZlecenieUpdateView2(generic.UpdateView):
     template_name = 'Zlecenie_app/zlecenie_form.html'
@@ -130,11 +117,3 @@ class ZlecenieCreateView(generic.CreateView):
         # def get(self, request, *args, **kwargs):
         #     return self.render_to_response(
         #         {'adresform': AdresForm(prefix='adresform_pre'), 'zlecenieform': ZleceniaForm(prefix='zlecenieform_pre')})  #
-
-
-@method_decorator(login_required, name='dispatch')
-class ZlecenieCreate_newAdesView(generic.CreateView):
-    model = Adres
-    template_name = 'Zlecenie_app/adres_form.html'
-    success_url = reverse_lazy('Zlecenie_app:nowe_zlecenie')
-    fields = ['nazwa', 'ulica', 'nr', 'kod_pocztowy', 'miasto']
